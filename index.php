f4663fc127590950d51ed837419c9aaf51453c5c
Marcin <marcin96semkowski@gmail.com>
2020-01-14 17:25:08 +0100

    #1

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+
+<head>
+
+     <meta charset="utf-8"/>
+    <title>Zadanie zdalne e-MSI </title>
+    <meta name="description" content="JS Memory Game inspired by Gewnt">
+    
+    <meta http-equiv="X-Ua-Compatibile" content="IE=edge.chrome=1">
+     
+    </head>
+
+<body>
+    
+    <div id="Lewy">
+     <ol type="a">
+    <li><a href="">Różne kontrolki HTML </a></li>
+         <li><a href="">Tabela Pracowników  </a></li>
+         <li><a href="">Tabela Faktur VAT  </a></li>
+         <li><a href="">Tabela Delegacji BD  </a></li>
+         <li>  <a href="">Dane Kontrahentów   </a></li>
+ </ol>     
+    
+    </div>
+    <div id="Prawy"></div>
+    
+    <div style="clear:both"></div>
+    
+    </body>
+</html>
